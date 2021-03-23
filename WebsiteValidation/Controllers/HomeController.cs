fc9f545e1b90eb7f92c44ded9efc34d24cb3ab7b
Eric <ericstalcup@gmail.com>
2021-03-23 17:18:27 -0500

    Add project files.

---
+++ WebsiteValidation/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebsiteValidation.Models;
+
+namespace WebsiteValidation.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index(RegistrationModel registration)
+		{
+			return View();
+		}
+	}
+}
---
+++ WebsiteValidation/Models/RegistrationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteValidation.Models
+{
+	public class RegistrationModel
+	{
+		[Required(ErrorMessage = "Enter a name.")]
+		[StringLength(30, ErrorMessage = "Name must be at least 30 characters")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Enter an address.")]
+		public string Address { get; set; }
+
+		[Required(ErrorMessage = "Enter a phone number.")]
+		[StringLength(10, ErrorMessage = "Phone number cannot be more than 10 numbers.")]
+		public string Phone { get; set; }
+
+		[Required(ErrorMessage = "Enter an email address.")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Enter an age.")]
+		public int Age { get; set; }
+	}
+}
